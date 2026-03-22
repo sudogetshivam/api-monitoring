96ac756735f1f9173d0571db8ce871c6005b0591
sudogetshivam <sorealgalaxy@gmail.com>
2026-03-22 23:56:46 +0530

    creating more util functions specially for handling errors

---
+++ src/shared/utils/appError.js
@@ -0,0 +1,12 @@
+//to handle internal servers error
+
+class AppError extends Error{
+    constructor(message, statusCode = 500, error = null){
+        //!never use this. before super in child class because as a child class untill and unless the parent class is not called the object insatnce doesnt exist
+        super(message) //this will say to its parent class which is Error, that use this message inside your constructor
+        this.statusCode
+        this.isOperational = true;
+        Error.captureStackTrace(this, this.constructor)
+
+    }
+}
