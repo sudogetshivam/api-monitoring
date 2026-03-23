2ac1bd3a539e574b603f13400ab328ad8016074d
sudogetshivam <sorealgalaxy@gmail.com>
2026-03-24 00:29:02 +0530

    started to intialise postgress for the endpoints metrics

--- server/scripts/init-postgres.sql
+++ server/scripts/init-postgres.sql
@@ -4,6 +4,9 @@ CREATE TABLE IF NOT EXISTS endpoint_metrics{
     service_name VARCHAR(255) NOT NULL,
     endpoint VARCHAR(500) NOT NULL,/* hitted which endpoint like requests are coming on which endpoint*/
     method VARCHAR(10) NOT NULL, /*which method used get,post*/
+    --if any requests comes suppose at 10;25 then its timestamp is taken and data/requests is stored inside a bucket whose bucket size is [10:00-11:00]
+    -- the aggragted matrix that we will  of whole bucket
+    --mhuje 1 ghante ki bucket do ham eska aggregated nikal lenge, esse kya hoga ki ham abde data main cheeze mesaure kar payenge
     time_bucket TIMESTAMPS NOT NULL,
     total_hits INTEGER DEFAULT 0,
     error_hits INTEGER DEFAULT 0, /*client ka aisa api jo user ne use kiya aur usko error mila */
