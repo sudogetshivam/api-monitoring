d0b45fb67cbc696df16c4fd3facc921596c61005
sudogetshivam <sorealgalaxy@gmail.com>
2026-03-23 23:44:50 +0530

    restructuring the code base

---
+++ server/scripts/init-postgres.sql
@@ -0,0 +1,18 @@
+CREATE TABLE IF NOT EXISTS endpoint_metrics{
+    id SERIAL PRIMARY KEY, 
+    client_id VARCHAR(24) NOT NULL, /*ye hame mongodb se milega*/
+    service_name VARCHAR(255) NOT NULL,
+    endpoint VARCHAR(500) NOT NULL,/* hitted which endpoint like requests are coming on which endpoint*/
+    method VARCHAR(10) NOT NULL, /*which method used get,post*/
+    time_bucket TIMESTAMPS NOT NULL,
+    total_hits INTEGER DEFAULT 0,
+    error_hits INTEGER DEFAULT 0, /*client ka aisa api jo user ne use kiya aur usko error mila */
+    avg_latency NUMERIC(10,3) DEFAULT 0.000,
+    min_latency NUMERIC(10,3) DEFAULT 0.000,
+    max_latency NUMERIC(10,3) DEFAULT 0.000,
+    created_at TIMESTAMPS DEFAULT CURRENT_TIMESTAMPS,
+    updated_at TIMESTAMPS DEFAULT CURRENT_TIMESTAMPS,
+
+    UNIQUE(client_id,service_name, endpoint, method, time_bucket);
+}
+
